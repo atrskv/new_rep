4be43236f84d37c2974c83db11b453ef1a5e22cf
Aleksei <trrskv@gmail.com>
2023-01-20 15:24:22 +0300

    added

--- test_skipped.py
+++ test_skipped.py
@@ -1,17 +1,17 @@
-# import pytest
-#
-# @pytest.mark.skip
-# def test_skipped1():
-#     pass
-#
-# @pytest.mark.skip
-# def test_skipped2():
-#     pass
-#
-# @pytest.mark.skip
-# def test_skipped3():
-#     pass
-#
-# @pytest.mark.skip
-# def test_skipped4():
-#     pass
+import pytest
+
+@pytest.mark.skip
+def test_skipped1():
+    pass
+
+@pytest.mark.skip
+def test_skipped2():
+    pass
+
+@pytest.mark.skip
+def test_skipped3():
+    pass
+
+@pytest.mark.skip
+def test_skipped4():
+    pass
