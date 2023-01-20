60fcbfdddfa6063046dbe012c11ad174138b3034
Aleksei <trrskv@gmail.com>
2023-01-20 15:28:53 +0300

    tests pass

--- test_pass.py
+++ test_pass.py
@@ -1,11 +1,11 @@
-def test_pass1():
-    pass
-
-def test_pass2():
-    pass
-
-def test_pass3():
-    pass
-
-def test_pass4():
-    pass
+# def test_pass1():
+#     pass
+#
+# def test_pass2():
+#     pass
+#
+# def test_pass3():
+#     pass
+#
+# def test_pass4():
+#     pass
